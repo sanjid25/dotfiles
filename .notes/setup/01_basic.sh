c6976fc4f67f1d2dc3c28bbbceb67700e6efcc91
sanjidnet <contact@sanjid.net>
2023-05-28 23:52:04 +1200

    convert ansible playbook template to shell scripts

---
+++ .notes/setup/01_basic.sh
@@ -0,0 +1,9 @@
+# MUST DO STEPS
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+sudo apt-get install vim-gtk3 -y # vim with clipboard
+sudo apt-get install bash
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
---
+++ .notes/setup/03_secondary.sh
@@ -0,0 +1,5 @@
+# FOLLOW UP STEPS
+sudo apt-get install tmux
+sudo apt-get install vim plugins
+sudo apt-get install python
+sudo apt-get install rbenv
