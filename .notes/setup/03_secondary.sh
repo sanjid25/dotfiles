76080cca8ab5dd1a3a9408dd36db14294e286620
sanjidnet <contact@sanjid.net>
2024-05-09 21:37:55 +1200

    add more programming and cloud items for the setup

--- .notes/setup/03_secondary.sh
+++ .notes/setup/03_secondary.sh
@@ -1,7 +1,15 @@
-# FOLLOW UP STEPS
+# utils
+install ripgrep
+
+# tools
 sudo apt-get install tmux
 sudo apt-get install vim plugins
+
+# language
 sudo apt-get install python
 sudo apt-get install rbenv
+install R
 
-# install R
+# cloud
+install awscli
+install aws-vault
